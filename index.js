c0f2857dd0b53450ab95c4ebb46cdddad16691a1
Mac <tmcpbmay@gmail.com>
2020-09-28 12:29:44 -0400

    colors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #! /usr/bin/env node
 
+const colors = require('colors')
 const fs = require('fs')
 
 fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
     if (err) {
         console.log(err)
     } else {
-        console.log(data)
+        console.log(colors.rainbow(data))
         return data
     }
 })
